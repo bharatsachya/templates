be81973d50f3109f59e871db610b87b10f197ee9
Ganesh <ganeshmangla2003@gmail.com>
2025-04-01 22:33:03 +0530

    Image config

--- project-1/app/page.tsx
+++ project-1/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       {/* Hero Section */}
       <section className="relative h-screen">
         <Image
-          src="/placeholder.svg?height=1080&width=1920"
+          src="https://images.unsplash.com/photo-1618083707368-b3823daa2726?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Mountain lake with person"
           fill
           priority
